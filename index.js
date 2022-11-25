0c4077954c2a4b0a77cb954d966585e641628661
joy <letimjoy7@gmail.com>
2022-11-25 13:21:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1=31
+const num2=2
+const multiply = num1 * num2;
+console.log(num1 * num2);
+
+const random = 10
+console.log(random > 0);
+
+const num3=5
+const num4=34
+let mod = num4 % num3
+
+const max=Math.max(5, 14, 3, 20);
+
+
